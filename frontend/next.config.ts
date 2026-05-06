f43db4c3ae870c73e659f9be368202632116a115
HMean0309 <HMean0309@gmail.com>
2026-05-06 10:43:54 +0700

    fix config FE

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://13.212.10.167/api/:path*', // Proxy ngầm gọi sang EC2
+      },
+    ];
+  },
 };
 
 export default nextConfig;
